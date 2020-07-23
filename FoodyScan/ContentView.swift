b1bbb36039bbffcb463bd642b2d14ce9867b57e0
Noe <noedelacroix@gmail.com>
2020-07-23 11:28:55 +0100

    Initial Commit

---
+++ FoodyScan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodyScan
+//
+//  Created by Noe De La Croix on 23/07/2020.
+//  Copyright © 2020 Noe De La Croix. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
